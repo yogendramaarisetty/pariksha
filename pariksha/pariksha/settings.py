df234c9da70978c168900d7fc646b2a82f4e0573
yogendramaarisetty <yogendramaarisetty@gmail.com>
2020-03-18 10:34:30 +0530

    settings

--- pariksha/pariksha/settings.py
+++ pariksha/pariksha/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'pariksha',
         'USER':'postgres',
-        'PASSWORD':'yogi',
+        'PASSWORD':'postgres',
         'HOST':'localhost',
         'PORT':'5432',
     }
