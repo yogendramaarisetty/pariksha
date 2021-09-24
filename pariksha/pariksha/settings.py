f44726bbfc199eb74a0a81ea31a9fb27b3fe237a
ymaarisetty_ha <ymaarisetty@hostanalytics.com>
2021-09-25 02:52:06 +0530

    settings new

--- pariksha/pariksha/settings.py
+++ pariksha/pariksha/settings.py
@@ -144,6 +144,9 @@ MEDIAL_URL = '/media/'
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 LOGIN_REDIRECT_URL = 'tests'
 
-CELERY_ACCEPT_CONTENT = ['json']
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_BACKEND = 'django-db'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIME_ZONE = 'Asia/Kolkata'
