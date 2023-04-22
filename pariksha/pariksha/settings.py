a89ed4dd49591c984778ff9992a1ca358eb7e2df
Yogendra Maarisetty <yogendra.maarisetty@wisetechglobal.com>
2023-04-22 17:51:41 +0530

    updated requirements.txt

--- pariksha/pariksha/settings.py
+++ pariksha/pariksha/settings.py
@@ -91,7 +91,7 @@ WSGI_APPLICATION = 'pariksha.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'pariksha',
+        'NAME': 'postgres',
         'USER':'postgres',
         'PASSWORD':'postgres',
         'HOST':'localhost',
@@ -128,7 +128,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
